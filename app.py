f4be1e61520d8e4d5afa43272d81b7e2a5f4f521
3302Banyumas <bps.banyumas@gmail.com>
2026-03-10 11:26:07 +0700

    Change Google Sheets URL in app.py
    Updated Google Sheets URL for data retrieval.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Data Rekap SPM 2025 BPS Kabupaten Banyumas")
 # --- AMBIL DATA DARI GOOGLE SHEET ---
 # Ganti link berikut dengan link CSV dari Google Spreadsheet
 # Caranya: File -> Share -> Publish to web -> pilih CSV
-sheet_url = "https://docs.google.com/spreadsheets/d/1WJR1tCiYRCJYbvfcW8fMySuqzG55QoHdsI7gZsiN_a0/export?format=csv"
+sheet_url = "https://docs.google.com/spreadsheets/d/1EyqXe1WcPCvi1LvkOiYVQ-nKdLMZu0mVNiHJxae-cH0/export?format=csv"
 df = pd.read_csv(sheet_url)
 
 # --- FITUR PENCARIAN ---
@@ -22,5 +22,6 @@ else:
 
 # --- TAMPILKAN TABEL ---
 st.dataframe(df_filtered, use_container_width=True)
+
 
 
